a14f6ae0bdc29aa3f0264d4258bd3c546c865215
ireneo.gervasoni <ireneo.gervasoni@eurobet.it>
2023-10-11 16:04:19 +0200

    added link into card component

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from "react";
 import '../scss/components/card.scss';
+import { Link } from "gatsby"
 
 
-export const CardComponent = ({name, description, img})=> {
+export const CardComponent = ({id, name, description, img})=> {
     return <div className="card">
-        {name} 
+        {name}
+        <Link to={`/playlist-${id}`}>{name}</Link>
     </div>
 }
